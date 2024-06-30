beed973323d3218feb7b7ca1983a0d05dde05916
mdzwuzyka <dsw52745@student.dsw.edu.pl>
2024-06-30 18:28:03 +0200

    Dodanie opcji --logs do skryptu

--- skrypt.sh
+++ skrypt.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
-# Sprawdzenie, czy argument '--date' został przekazany
-if [ "$1" == "--date" ]; then
-    # Wyświetlenie dzisiejszej daty
+show_date() {
     echo "Dzisiejsza data: $(date '+%Y-%m-%d')"
-fi
+}
+
+create_logs() {
+    for i in {1..100}; do
+        filename="log_$i.txt"
+        echo "Numer pliku: $i" > "$filename"
+        echo "Nazwa skryptu: $0" >> "$filename"
+        echo "Data utworzenia: $(date '+%Y-%m-%d %H:%M:%S')" >> "$filename"
+    done
+    echo "Utworzono 100 plików log_x.txt"
+}
+
+# Sprawdzenie przekazanych argumentów
+case "$1" in
+    --date)
+        show_date
+        ;;
+    --logs)
+        create_logs
+        ;;
+    *)
+        echo "Użycie: $0 --date | --logs"
+        ;;
+esac
